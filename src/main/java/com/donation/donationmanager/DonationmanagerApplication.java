070763de726b73db9f286060b7d35e27ba1403fd
Amol-Choudhari <choudhari.amol87@gmail.com>
2024-03-15 18:23:49 +0530

    first commit

---
+++ src/main/java/com/donation/donationmanager/DonationmanagerApplication.java
@@ -0,0 +1,14 @@
+package com.donation.donationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationmanagerApplication.class, args);
+	}
+
+}
+ 
---
+++ src/test/java/com/donation/donationmanager/DonationmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donation.donationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
