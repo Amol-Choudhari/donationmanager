2b7675822d8ba626b5bde69774c28672e1e5c9a8
Amol-Choudhari <choudhari.amol87@gmail.com>
2024-05-22 17:49:02 +0530

    updated API for add/edit user with response entity

--- src/main/java/com/donation/donationmanager/controller/UserController.java
+++ src/main/java/com/donation/donationmanager/controller/UserController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,11 +31,22 @@ public class UserController {
 	}
 	
 	//@PostMapping(value="adduser",consumes="application/json")
-	@PostMapping("adduser")
+	/*@PostMapping("adduser")
 	public void addUser(@RequestBody User user){
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		userrepository.save(user);		 
-	}
+	}*/
+	@PostMapping("adduser")
+    public ResponseEntity<Boolean> addUser(@RequestBody User user) {
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userrepository.save(user);
+            return ResponseEntity.ok(true); // User added successfully
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.ok(false); // Error occurred while adding user
+        }
+    }
 	
 	@GetMapping("getuser/{id}")
 	public User getUser(@PathVariable("id") Long id){
@@ -42,18 +54,26 @@ public class UserController {
 	}
 	
 	@PutMapping("edituser/{id}")
-	public void editUser(@PathVariable("id") Long id,@RequestBody User user){
-		User existingUser = userrepository.findById(id);
-        if (existingUser != null) {
-        	
-        	//set values from existing record to fields which will not be updated through request data
-        	user.setId(id);
-        	user.setCreated(existingUser.getCreated());
-        	user.setModified(LocalDate.now());
-        	user.setPassword(existingUser.getPassword());
-        	
-        	//rest fields will be updated from the request data
-        	userrepository.update(user);
+	public ResponseEntity<Boolean> editUser(@PathVariable("id") Long id,@RequestBody User user){
+		try {
+			User existingUser = userrepository.findById(id);
+	        if (existingUser != null) {
+	        	
+	        	//set values from existing record to fields which will not be updated through request data
+	        	user.setId(id);
+	        	user.setCreated(existingUser.getCreated());
+	        	user.setModified(LocalDate.now());
+	        	user.setPassword(existingUser.getPassword());
+	        	
+	        	//rest fields will be updated from the request data
+	        	userrepository.update(user);
+	        	return ResponseEntity.ok(true); // User updated successfully
+	        }else {
+	        	return ResponseEntity.ok(false);//user not found
+	        }
+		} catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.ok(false); // Error occurred while adding user
         }
 	}
 	
